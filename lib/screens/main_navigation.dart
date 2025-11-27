e0a167ac8cff5e03f54f3f320b65aa01df49081b
Bengi <114860608+Egemen-Bengi@users.noreply.github.com>
2025-11-27 21:59:13 +0300

    profil sayfası yapıldı sayfalar arası geçiş bottom navigation bar ile yapıldı

---
+++ lib/screens/main_navigation.dart
@@ -0,0 +1,57 @@
+import 'package:cift_teker_front/screens/event_create_page.dart';
+import 'package:cift_teker_front/screens/profil_page.dart';
+import 'package:flutter/material.dart';
+
+class MainNavigation extends StatefulWidget {
+  const MainNavigation({super.key});
+
+  @override
+  State<MainNavigation> createState() => _MainNavigationState();
+}
+
+class _MainNavigationState extends State<MainNavigation> {
+  int _selectedIndex = 0;
+
+  final List<Widget> _pages = [
+    Center(child: Text("Ana Sayfa")),   // 0
+    ProfilePage(),                     // 1
+    EventCreatePage(),                 // 2
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  BottomNavigationBar _bottomNav() {
+    return BottomNavigationBar(
+      currentIndex: _selectedIndex,
+      selectedItemColor: Colors.orange,
+      unselectedItemColor: Colors.grey,
+      onTap: _onItemTapped,
+      items: const [
+        BottomNavigationBarItem(
+          icon: Icon(Icons.home),
+          label: "Home",
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.group),
+          label: "Profil",
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.add),
+          label: "Ekle",
+        ),
+      ],
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _pages[_selectedIndex],
+      bottomNavigationBar: _bottomNav(), // ← her sayfada bar var
+    );
+  }
+}
