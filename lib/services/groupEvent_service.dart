b28ad933fcb766c9064076dd71024b8f67e85bdf
volkanchi <volkan_celebii@hotmail.com>
2025-12-03 00:35:08 +0300

    Add city field to GroupEventRequest and GroupEventResponse; implement new Record and UpdateUsername requests and responses; enhance services for record management

--- lib/services/groupEvent_service.dart
+++ lib/services/groupEvent_service.dart
@@ -4,7 +4,9 @@ import '../models/responses/groupEvent_response.dart';
 import '../../core/models/api_response.dart';
 
 class EventService {
-  final Dio _dio = Dio(BaseOptions(baseUrl: "https://cift-teker-sosyal-bisiklet-uygulamasi.onrender.com"));
+  final Dio _dio = Dio(
+    BaseOptions(baseUrl: "https://cift-teker-sosyal-bisiklet-uygulamasi.onrender.com"),
+  );
 
   // Yeni grup etkinliği oluşturma
   Future<ApiResponse<GroupEventResponse>> createGroupEvent(
@@ -27,6 +29,18 @@ class EventService {
       "/event/me",
       options: Options(headers: {"Authorization": "Bearer $token"}),
     );
+
+    return ApiResponse.fromJson(
+      response.data,
+      (jsonList) => (jsonList as List)
+          .map((json) => GroupEventResponse.fromJson(json))
+          .toList(),
+    );
+  }
+
+  // Tüm grup etkinliklerini listeleme (public)
+  Future<ApiResponse<List<GroupEventResponse>>> getAllGroupEvents() async {
+    final response = await _dio.get("/event/all");
 
     return ApiResponse.fromJson(
       response.data,
